5f8703466256d1f9d95edaaeb200e0ab1ae8b38a
Neenu Chacko <45137335+neenu-chacko@users.noreply.github.com>
2022-08-30 10:56:26 +0530

    Added API based architecture

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
-    MAX_TITLE_LENGTH = 125
-    validates :title, presence: true, length : {maximum : MAX_TITLE_LENGTH}
+  # MAX_TITLE_LENGTH = 125
+  # , length : {maximum : MAX_TITLE_LENGTH}
+  validates :title, presence: true
 end
