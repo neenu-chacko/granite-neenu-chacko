bc39167a84cf5d58a95d4f186dadffd80a86247a
Neenu Chacko <45137335+neenu-chacko@users.noreply.github.com>
2022-08-30 15:35:19 +0530

    Added slug to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,28 @@
 class Task < ApplicationRecord
   # MAX_TITLE_LENGTH = 125
   # , length : {maximum : MAX_TITLE_LENGTH}
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = title.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Task.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, t("task.slug.immutable"))
+      end
+    end
 end
