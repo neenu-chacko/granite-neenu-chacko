17be777a35e4beeebca7e5581ad06d61b3701d51
Neenu Chacko <45137335+neenu-chacko@users.noreply.github.com>
2022-09-05 15:25:32 +0530

    Added unit tests for Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@
 
 class Task < ApplicationRecord
   RESTRICTED_ATTRIBUTES = %i[title task_owner_id assigned_user_id]
+  MAX_TITLE_LENGTH = 50
 
   enum progress: { pending: "pending", completed: "completed" }
   enum status: { unstarred: "unstarred", starred: "starred" }
@@ -9,7 +10,7 @@ class Task < ApplicationRecord
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
 
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :slug, uniqueness: true
   validate :slug_not_changed
 
